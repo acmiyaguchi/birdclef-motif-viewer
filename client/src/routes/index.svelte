d99a423e6bb492b286500076174d271e60039634
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-06-03 20:28:12 -0700

    Add CORS setting and test fetching from api

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,4 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import { browser } from "$app/env";
+  let version: string;
+  $: browser &&
+    fetch("http://localhost:8000/version")
+      .then((res) => res.json())
+      .then((data) => (version = data.version));
+</script>
+
+<h1>birdclef-motif-viewer</h1>
+
+{#if version}
+  <p>Backend version: {version}</p>
+{/if}
